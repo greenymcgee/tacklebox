c13d755ab8a0b2dbf1f798d1ac258125007cb38d
greenymcgee <greenhouston@gmail.com>
2026-02-14 15:34:02 -0700

    Consolidates Configs to Top Level

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,10 @@
 /* eslint-disable sort-keys */
-import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     coverage: {
-      include: ['src/'],
+      include: ['packages/'],
       provider: 'v8',
       thresholds: {
         branches: 97,
@@ -16,11 +15,6 @@ export default defineConfig({
     },
     globals: true,
     environment: 'node',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(import.meta.dirname, './src'),
-    },
   },
 })
 
